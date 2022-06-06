96ed3c32e602d133b6c346d269aa9edb1ab0bd48
root <root@DESKTOP-7RM4R3T.localdomain>
2022-06-06 21:30:34 +0300

    Initial files

---
+++ egfp/run-mm-nvt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -J egfp-mm-nvt
+#SBATCH --time=00:20:00
+#SBATCH --partition=standard
+#SBATCH --qos=standard
+#SBATCH --account=ta060
+#SBATCH --reservation=ta060_170
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=32
+#SBATCH --cpus-per-task=4
+
+#SBATCH --distribution=block:block 
+#SBATCH --hint=nomultithread
+
+module purge
+module use /work/ta060/ta060/shared/modulefiles/gromacs2022
+module load gmx_cp2k
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_PLACES=cores
+
+srun gmx_mpi_d mdrun -s egfp-mm-nvt.tpr -deffnm egfp-mm-nvt
